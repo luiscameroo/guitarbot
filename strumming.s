7b1fbf14b2ec9983b760e20ed547a9cd3ed5d6dd
luiscameroo <luiscameroo@users.noreply.github.com>
2018-03-19 22:19:19 -0400

    Strumming should work
    Corrected address of JP1

--- strumming.s
+++ strumming.s
@@ -1,13 +1,35 @@
-.equ LEGO 0xFF2000060
-.equ TIMER 0xFF202000
-.equ STACK 0x7FFFFFFF
-.equ HALF  0x003D0900
+.equ LEGO, 0xFF200060
+.equ TIMER, 0xFF202000
+.equ STACK, 0x7FFFFFFF
+.equ HALF,  0x003D0900
+
+.section .exceptions
+ISR:
+	movia r8, LEGO
+	ldwio et, (r8)
+	andi et, et, 0b010
+	beq et, r0, setreverse
+
+setforward:
+	ldwio et, (r8)
+	andi et, et, 0b1101
+	br done
+	
+setreverse:
+	ldwio et, (r8)
+	andi et, et, 0b1101
+
+done:
+	subi ea, ea, 4
+	eret
+
+
 .section .text
 
 .global _start
 _start:
 	#Initialize Motors 
-	movia r8, (LEGO)
+	movia r8, LEGO
 	movia r9, 0x07F557FF
 	stwio r9, 4(r8)
 	
@@ -30,23 +52,12 @@ _start:
 	andi r9, r9, 0xFFFF
 	stwio r9, 12(r8)
 	
-	movi r9, 0b111
+	movui r9, 0b111
 	stwio r9, 4(r8)
 	
 loop:
 	br loop
 	
-.section .exceptions 'ax'
-ISR:
-	movia r8, (LEGO)
-	ldwio et, (r8)
-	andi et, et, 0b010
-	beq et, r0, setreverse
-
-setforward:
-	ldwio et, (r8)
-	andi et, et, 0b1101
-	br done
 
 
 	
