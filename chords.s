3db5988f999f582cf483914eed4a22e84a1245a7
Lukasz Jagodzinski <ljagodz@Lukaszs-MacBook-Pro.local>
2018-03-30 15:58:07 -0400

    Began writing subroutines chord_down and chord_up to initiate pressing of chords

--- chords.s
+++ chords.s
@@ -1,2 +1,45 @@
-# CHORDS MODULE
+#------ DEVICE LOCATIONS ------#
+.equ LEGO, 0xFF200060 #Use Motor 1 of lego controller
+.equ TIMER1, 0xFF202000
+.equ TIMER2, 0xFF202020
+.equ STACK, 0x7FFFFFFF
+
+#------ MOTOR TIMING ------#
+.equ PRESS_TIME 0x00 #Currently uninitialized.
+
+####################################
+#=== CHORD MODULE ===#
+
+.section .text
+
+.global chord_down
+chord_down:
+# CALLEE-SAVE HERE
+
+# load LEGO IO address
+    movia r16, LEGO
+
+# set motor to forward, on (preserve other info)
+    ldw r17, (r16)
+    andi r17, r17, 0xFFF3 #(....0011)
+    stwio r17, (r16)
+
+# return to previous
+    ret
+
+.global chord_up
+chord_up:
+# CALLEE-SAVE HERE
+
+#load LEGO IO address
+    movia r16, LEGO
+
+# set motor to reverse, on (preserve other info)
+    ldw r17, (r16)
+    andi r17, r17, 0xFFFB #(....1011)
+    stwio r17, (r16)
+
+# return to previous
+    ret
+
 
