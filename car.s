2858c6b41c256b0a97a56e813da74d980b6c65ad
Lukasz Jagodzinski <ljagodz@Lukaszs-MacBook-Pro.local>
2018-03-30 19:18:14 -0400

    added car.s file for subroutines associated with controlling car movement

---
+++ car.s
@@ -0,0 +1,15 @@
+#------ DEVICE LOCATIONS ------#
+.equ LEGO, 0xFF200060 #Use Motor 1 of lego controller
+.equ TIMER1, 0xFF202000
+.equ TIMER2, 0xFF202020
+.equ STACK, 0x7FFFFFFF
+
+#------ MOTOR TIMING ------#
+.equ FRET1_TIME, 0x00
+.equ FRET2_TIME, 0x00
+.equ FRET3_TIME, 0x00
+.equ FRET4_TIME, 0x00
+.equ FRET5_TIME, 0x00
+.equ FRET6_TIME, 0x00
+.equ FRET7_TIME, 0x00
+
