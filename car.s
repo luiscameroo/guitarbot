9bff096754e6c1da2bee5c03fe78364d92128e29
ljagodz <ljagodz@gmail.com>
2018-04-03 01:04:18 -0400

    began writing car movement code in car.s

--- car.s
+++ car.s
@@ -5,22 +5,53 @@
 .equ STACK, 0x7FFFFFFF
 
 #------ MOTOR TIMING ------#
-.equ FRET1_TIME, 0x00
-.equ FRET2_TIME, 0x00
-.equ FRET3_TIME, 0x00
-.equ FRET4_TIME, 0x00
-.equ FRET5_TIME, 0x00
-.equ FRET6_TIME, 0x00
-.equ FRET7_TIME, 0x00
+.equ car_move_time, 200000000 #200 million
 
-#=== CAR MODULES ===#
+
+#=== INTERRUPT HANDLER ===#
+#NOTE: this is exclusive to timer1 for car.
+.section .exceptions, "ax"
+ISR:
+#clear timeout bit
+    movia et, TIMER1
+    stwio r0, et
+
+turn_off_motor2:
+    
 
 .section .text
+.global _start
 
-.global car_forward
-car_forward:
+_start:
 
-.global car_reverse
-car_reverse:
+initialize_stack:
+    movia sp, STACK
+
+initialize_motor2: #it is the 3rd motor associated with the car.
+    movia r8, LEGO
+#Parellel port- setting to default
+    movia r9, 0x07F557FF
+    stw r9, 4(r8)
+#Turn motor2 on
+    movi r9, 0b000101
+    stw r9, (r8)
+
+initialize_timer1:
+    movia r8, TIMER1
+#load car_move_time into timer (2 seconds)
+#load low 16 bits
+    movia r9, %lo(car_move_time)
+    andi r9, r9, 0xFFFF
+    stwio r9, 8(r8)
+#load top 16 bits
+    movia r9, %hi(car_move_time)
+    andi r9, r9, 0xFFFF
+    stwio r9, 12(r8)
+#enable interrupts, turn on timer, run once until timeout bit
+    movui r9, 0b101
+    stwio r9, 4(r8)
+
+loop:
+    br loop
 
 
