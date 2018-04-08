997b7afab5047a5fef201d1eb504e2770ff0f85b
ljagodz <ljagodz@gmail.com>
2018-04-07 22:28:08 -0400

    initialized keyboard for interrupts

--- keyboard.s
+++ keyboard.s
@@ -6,12 +6,28 @@
 .equ TIMER1, 0xFF202000
 .equ TIMER2, 0xFF202020
 .equ STACK, 0x00002000
+.equ 1PS2, 0xFF200100
 
 #=== DATA ===#
 .section .data
 
+
+
 #=== INTERRUPT SERVICE ROUTINE ===#
 .section . exceptions, "ax"
 
+
+
 #=== PROGRAM INSTRUCTIONS ===#
 .section .text
+
+#initialize stack
+movia sp, STACK
+
+#initialize keyboard interrupts (write 1 to 1st bit of control register)
+movia r8, 1PS2
+addi r9, r9, 1
+stw r9, 4(r8)
+
+
+
